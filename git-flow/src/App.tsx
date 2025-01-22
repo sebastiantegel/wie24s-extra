e50016ca92ae1fb7059e05eb3fcb909f781ed237
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-01-22 16:33:40 +0100

    Added todos list

--- git-flow/src/App.tsx
+++ git-flow/src/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
+class Todo {
+  constructor(public text: string) {}
+}
+
 function App() {
-  return <></>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  return (
+    <>
+      <ul>
+        {todos.map((t) => {
+          return <li>{t.text}</li>;
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default App;
