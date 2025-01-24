b2ce1e624143cf4b27e60c8f70a6aecea31b7ee7
stello <aramastelian@gmail.com>
2025-01-24 08:45:20 +0100

    Ändrat i footern

--- git-layout/src/App.tsx
+++ git-layout/src/App.tsx
@@ -5,7 +5,30 @@ function App() {
     <>
       <header></header>
       <main></main>
-      <footer></footer>
+      <footer>
+        <div className="contact-socials contact-content">
+          <ul className="icon-socials">
+            <li>
+              <a href="https://instagram.com" target="_blank">
+                <i className="fab fa-instagram"></i>
+              </a>
+              <span>Instagram</span>
+            </li>
+            <li>
+              <a href="https://twitter.com" target="_blank">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <span>Twitter</span>
+            </li>
+            <li>
+              <a href="https://facebook.com" target="_blank">
+                <i className="fab fa-facebook facebook"></i>
+              </a>
+              <span>Facebook</span>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </>
   );
 }
