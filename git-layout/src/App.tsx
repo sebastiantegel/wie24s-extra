66278235ab57886d3b1d7fe7d447fb38354fcc1e
Jessika Karlsson <jessikakarlsson@MacBook-Pro-som-tillhor-Jessika.local>
2025-01-24 08:45:06 +0100

    footer contact

--- git-layout/src/App.tsx
+++ git-layout/src/App.tsx
@@ -5,7 +5,20 @@ function App() {
     <>
       <header></header>
       <main></main>
-      <footer></footer>
+      <footer>
+      <div className="footer-contact">
+          <h3>Contact Us</h3>
+          <p>
+            Email: info@gitflow.com
+          </p>
+          <p>Phonenumber: 0701234567</p>
+        </div>
+        <div className="footer-address">
+        <h3>Address</h3>
+        <p>Gustavslundsvägen 151</p>
+        <p>167 51 Bromma</p>
+      </div>
+      </footer>
     </>
   );
 }
