eb89ac92312d41eecb36f45a131e7f7d4664a0ea
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-01-31 08:54:18 +0100

    Refactored styling

--- git-layout/src/App.tsx
+++ git-layout/src/App.tsx
@@ -1,28 +1,13 @@
 import "./App.scss";
 import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 import "./index.scss";
 // import logo from "./assets/bilder/jordan-logo-bilder.jpg";
 
 function App() {
   return (
     <>
-      <header>
-        <h2>Lorem ipsum</h2>
-        <div className="logoTyp">{/* <img src={logo} alt="" /> */}</div>
-        <nav>
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Header />
       <main>This is the main content for the page</main>
       <Footer />
     </>
